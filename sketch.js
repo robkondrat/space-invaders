57566e080973ed7dd8bd4fdfee31ad2dede05ddc
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-07-14 12:48:11 -0500

    player controls

--- sketch.js
+++ sketch.js
@@ -1,3 +1,13 @@
 function mousePressed() {
   invaders.checkCollision(mouseX, mouseY);
 }
+
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW || keyCode == 88) {
+    player.moveRight();
+  } else if (keyCode === LEFT_ARROW || keyCode == 90) {
+    player.moveLeft();
+  } else if (keyCode === 32) {
+    player.shoot();
+  }
+}
